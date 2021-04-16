a5217df776f9e8564df3ad7dd5206c3b8872be46
Manish Bharti <manish2bharti@users.noreply.github.com>
2021-04-16 07:35:33 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -7,3 +7,24 @@ function fibonacci(position) {
 }
 
 console.log(fibonacci(100));
+
+//-------------------------------------
+
+function fibMemo(index, cache) {
+    // index: index of num in fibonacci sequence
+    // cache:  an array used as memory
+
+    cache = cache || [];
+    if(cache[index]){
+        return cache[index];
+    }else{
+        if(index < 3){
+            return 1;
+        }else{
+            cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache)
+        }
+    }
+    return cache[index];
+}
+
+console.log(fibMemo(1000));
