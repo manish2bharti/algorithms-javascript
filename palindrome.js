20638c729e9bc10a41f8d34659ce487c71bf584c
Manish Bharti <manish2bharti@users.noreply.github.com>
2021-05-16 18:09:22 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -12,3 +12,20 @@ function isPalindrome(string) {
 }
  
 isPalindrome("Madam, I'm Adam");
+
+///////////////////////////////////////////////////////////////////////
+
+function isPalindrome(word){
+   var i,wLength = word.length-1,wLengthToCompare = wLength/2;
+
+   for (i = 0; i <= wLengthToCompare ; i++) {
+     if (word.charAt(i) != word.charAt(wLength-i)) {
+     		console.log(false)
+        return false;
+     }
+   }
+   console.log(true)
+   return true;
+} 
+
+isPalindrome("mam")
